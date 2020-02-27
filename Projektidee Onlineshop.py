9bcce55d7e6eec7caa13ab03a2168211eb95f5aa
Selina-DBM <selina.marty@gmail.com>
2020-02-27 14:24:58 +0100

    Projektidee Aktualisierung

--- Projektidee Onlineshop.py
+++
@@ -1,8 +0,0 @@
-Onlineshop
-
-Die Webseite wird geöffnet und als erstes wird direkt nach dem Namen des Users gefragt. Anschliessend wird immer mit diesem Namen einen persönlichen Bezug geschaffen. 
-Die Seite fragt dann ab, an welchen Kleiderstile, Farben und Grössen man interessiert ist. Diese Fragen sind nicht in Form eines klassischen Filters, sondern eine Abfrage z.B.:
-Welche Kleidungsstücke interessieren dich?
-Auswahl: Hosen, T-Shirts, Kleider etc. 
-
-Dem Kunden werden dann nur noch Kleidungsstücke angezeigt, die der Filterung entsprechen und dem Kunden grundsätzlich passen könnten. Der Kunde kann sich dann die Kleidungsstücke aussuchen und in den Warenkorb legen. Anschliessend berechnet die Website die Versandkosten. 
