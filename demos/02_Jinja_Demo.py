3de388b9dff6c813dcaf97486ec67c7cf591a8a9
Selina-DBM <selina.marty@gmail.com>
2020-03-06 13:54:29 +0100

    Div. Änderungen

--- demos/02_Jinja_Demo.py
+++ demos/02_Jinja_Demo.py
@@ -1,17 +1,28 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask("Hello World")
 
+@app.route("/index/", methods=['GET', 'POST'])
+def hallo():
+    if request.method == 'POST':
+        vorname = request.form['vorname']
+        return render_template("seite1.html")
+    return render_template('index.html')
 
-@app.route('/hello')
-def hello_world():
-	return render_template('index.html', name="Fabian")
 
 
-@app.route("/test")
-def test():
-    return "success"
+
+@app.route('/seiteeins')
+def seiteeins():
+    return render_template("seite1.html")
+
+
+
+@app.route('/seitezwei')
+def seitezwei():
+    return render_template("seite2.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
